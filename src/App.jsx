a2f14b7cf0c263c0f864c5cfa4397683403b2520
merint0414-dot <merint0414@gmail.com>
2026-07-15 02:08:43 +0530

    View Booking Station

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import AddStation from './components/AddStation'
+import ViewStation from './components/ViewStation'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <AddStation/>
+      <ViewStation/>
     </>
   )
 }
